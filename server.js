7168ab1852f5d11535c898df803cdc7a8516c5e3
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-05-19 12:13:12 +0530

    updated

--- server.js
+++ server.js
@@ -21,23 +21,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
- app.use(cookieParser());
+app.use(cookieParser());
 
-// const allowedOrigins = ['http://localhost:3001' || 'https://main--enchanting-piroshki-6c4c3f.netlify.app/']; app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
-//     }
-//         if(allowedOrigins.indexOf(origin) === -1) {
-//     const msg = 'The CORS policy for this site does not allow access from the specified origin.'; return callback(new Error(msg)
-//         }, true)
-
-app.use(express.static('public', {
-    setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-        }
-    }
-}));
 
 // user routes
 app.use("/api/user", userRoute);
