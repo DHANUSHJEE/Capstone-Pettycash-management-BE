5030e2efc16938c2618208dc53648f04417f143b
Dhanush J S <dhanushjayakumar2@gmail.com>
2024-05-19 12:24:17 +0530

    updated

--- server.js
+++ server.js
@@ -1,3 +1,46 @@
+// import express from "express";
+// import cors from "cors";
+// import morgan from "morgan";
+// import dotenv from "dotenv";
+// import { connectDB } from "./config/connectDb.js";
+// import userRoute from "./routes/userRoute.js";
+// import transactionRoute from "./routes/transactionRoute.js";
+// import cookieParser from "cookie-parser";
+// import passwordResetRoute from "./routes/passwordReset.js";
+
+// // dotenv config
+// dotenv.config();
+
+// // port
+// const port = process.env.PORT
+
+// // rest of the code
+// const app = express();
+
+// // middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan("dev"));
+// app.use(cookieParser());
+
+
+// // user routes
+// app.use("/api/user", userRoute);
+
+// // transaction routes
+// app.use("/api/transaction", transactionRoute);
+
+// // password reset routes
+// app.use("/api/password-reset", passwordResetRoute);
+
+
+// // listen server
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`.yellow.bold);
+// });
+
+// connectDB();
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -7,36 +50,53 @@ import userRoute from "./routes/userRoute.js";
 import transactionRoute from "./routes/transactionRoute.js";
 import cookieParser from "cookie-parser";
 import passwordResetRoute from "./routes/passwordReset.js";
+import path from "path";
 
 // dotenv config
 dotenv.config();
 
-// port
-const port = process.env.PORT  
+// Ensure PORT is defined in .env
+if (!process.env.PORT) {
+    console.error("PORT is not defined in the .env file");
+    process.exit(1);
+}
 
-// rest of the code
+// port
+const port = process.env.PORT ; 
+
+// Initialize express app
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
+// Serve static files with correct MIME types
+app.use('/static', express.static(path.join(__dirname, 'public'), {
+    setHeaders: function (res, path) {
+        if (path.endsWith('.js')) {
+            res.setHeader('Content-Type', 'application/javascript');
+        }
+    }
+}));
 
-// user routes
+// API routes
 app.use("/api/user", userRoute);
-
-// transaction routes
 app.use("/api/transaction", transactionRoute);
-
-// password reset routes
 app.use("/api/password-reset", passwordResetRoute);
 
-
-// listen server 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`.yellow.bold);
+// Error handling middleware (optional, but recommended)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
+// Listen server 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Connect to the database
 connectDB();
